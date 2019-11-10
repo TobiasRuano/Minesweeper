10bcf4c4ae60f27bb52bb35a9e2e74e37a7dace8
Tobias Ruano <ruano.t10@gmail.com>
2019-11-10 11:37:05 -0300

    update

--- src/Main/buscaMinas.java
+++ src/Main/buscaMinas.java
@@ -13,11 +13,11 @@ public class buscaMinas {
 
 	static class celda {
 		char valor;
-		boolean visible;
+		boolean visitado;
 		
 		public celda() {
 			this.valor = 'X';
-			this.visible = false;
+			this.visitado = false;
 		}
 	}
 
@@ -54,7 +54,7 @@ public class buscaMinas {
 					caracter = (char) br.read();
 					if (caracter != '\n' && caracter != ' ') {
 						matrizInicial[i][j].valor = caracter;
-						matrizInicial[i][j].visible = false;
+						matrizInicial[i][j].visitado = false;
 						System.out.print(matrizInicial[i][j].valor);
 					} else {
 						j--;
@@ -63,7 +63,6 @@ public class buscaMinas {
 				System.out.print("\n");
 			}
 			System.out.print("\n");
-			
 			fr.close(); // termino la lectura del archivo, por ende lo cierro
 
 			ArrayList<par> resultado;
@@ -80,25 +79,22 @@ public class buscaMinas {
 					for(a = 0; a < matrizInicial.length; a++) {
 						  for(b = 0; b < matrizInicial[a].length; b++) {
 							  copiaMatriz[a][b].valor = matrizInicial[a][b].valor;
-							  copiaMatriz[a][b].visible = matrizInicial[a][b].visible;
+							  copiaMatriz[a][b].visitado = matrizInicial[a][b].visitado;
 						  }
 					}
-					
 					ArrayList<par> listaClicks = new ArrayList<par>();
 					ArrayList<par> listaEntrada = new ArrayList<par>();
-					
 					for(a = 0; a < resultado.size(); a++)
 						listaEntrada.add(a, resultado.get(a));
 					resultado = juego(listaEntrada, listaClicks, copiaMatriz, i, j, bombas);
 				}
 			}
-
 			System.out.print("Se encontro la solucion mas optima en " + resultado.size() + " clicks. \n");
 			for (i = 0; i < resultado.size(); i++) {
 				System.out.print(resultado.get(i).primer + "," + resultado.get(i).segundo + "\n");
 			}
 		} catch(IOException e) {
-			System.out.print("No se ha podido leer el archivo");
+			System.out.print("Error al ejecutar el algoritmo");
 			e.printStackTrace();
 		}
 	}
@@ -108,7 +104,7 @@ public class buscaMinas {
 		int columnas = matriz[0].length;
 		int i = 0, j = 0, pendientes;
 
-		if(matriz[posF][posC].valor != 'X' && matriz[posF][posC].visible == false) {
+		if(matriz[posF][posC].valor != 'X' && matriz[posF][posC].visitado == false) {
 			
 			par casilla = new par();
 			casilla.primer = posF;
@@ -118,7 +114,6 @@ public class buscaMinas {
 			matriz = revelar(matriz, posF, posC);
 
 			pendientes = contarPendientes(matriz);
-
 			if(cantBombas == pendientes) { // Aca llegue a una hoja, por ende chequeo si los clicks actuales son o no la mejor solucion
 				if(listaClicks.size() < listaSolucion.size() || listaSolucion.size() == 0) {
 					int a;
@@ -142,37 +137,28 @@ public class buscaMinas {
 	}
 
 	private static celda[][] revelar(celda[][] matriz, int fila, int columna) {
-		matriz[fila][columna].visible = true;
-
+		matriz[fila][columna].visitado = true;
 		if(matriz[fila][columna].valor == '0') {
-			celda[][] copia = revelarAdyacentes(matriz, fila, columna);
-			matriz = Arrays.stream(copia).map(celda[]::clone).toArray(celda[][]::new);
-		}
-		return matriz;
-	}
-
-	private static celda[][] revelarAdyacentes(celda[][] matriz, int fila, int columna) {
-		int i = fila - 1, j = columna - 1;
-		while(i <= fila + 1) {
-			j = columna - 1;
-			while(j <= columna + 1) {
-				if(i >= 0 && i < matriz.length && j >= 0 && j < matriz[0].length && matriz[i][j].visible == false) {
-					celda[][] copia = revelar(matriz, i, j);
-					matriz = Arrays.stream(copia).map(celda[]::clone).toArray(celda[][]::new);
+			int i = fila - 1, j = columna - 1;
+			while(i <= fila + 1) {
+				j = columna - 1;
+				while(j <= columna + 1) {
+					if(i >= 0 && i < matriz.length && j >= 0 && j < matriz[0].length && matriz[i][j].visitado == false) {
+						matriz = revelar(matriz, i, j);
+					}
+					j++;
 				}
-				j++;
+				i++;
 			}
-			i++;
 		}
 		return matriz;
 	}
 
 	private static int contarPendientes(celda[][] matriz) {
 		int i, j, cant = 0;
-
 		for(i = 0; i < matriz.length; i++) {
 			for(j = 0; j < matriz[0].length; j++) {
-				if(matriz[i][j].visible == false) {
+				if(matriz[i][j].visitado == false) {
 					cant ++;
 				}
 			}
